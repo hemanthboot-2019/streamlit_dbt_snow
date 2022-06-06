34790dbea9393717b97df306b9252b35304c5c06
hemanthboot-2019 <49744737+hemanthboot-2019@users.noreply.github.com>
2022-06-06 21:19:22 +0530

    Update page1.py

--- pages/page1.py
+++ pages/page1.py
@@ -1,1 +1,25 @@
 
+
+import streamlit as st
+import pandas as pd
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
+st.set_page_config(
+     page_title="AI-Support",
+     page_icon="❄️",
+     layout="wide",
+     initial_sidebar_state="expanded",
+     menu_items={
+         'Get Help': 'https://www.extremelycoolapp.com/help',
+         'Report a bug': "https://www.extremelycoolapp.com/bug",
+         'About': "# This is a header. This is an *extremely* cool app!"
+     }
+ )
+#@streamlit.cache  
+
+my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+with my_cnx.cursor() as my_cur:
+     my_cur.execute("select * from DEV_RAW.PUBLIC.DBT_MAPPING")
+     st.dataframe(my_cur.fetchall())
