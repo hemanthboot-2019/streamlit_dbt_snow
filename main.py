ffa344e184425a05d1e45d25924bc83332cd7af5
hemanthboot-2019 <49744737+hemanthboot-2019@users.noreply.github.com>
2022-06-06 19:14:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,9 +16,17 @@ st.set_page_config(
          'About': "# This is a header. This is an *extremely* cool app!"
      }
  )
+@streamlit.cache  
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+def get_clean_count():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("select * from DEV_RAW.PUBLIC.DBT_MAPPING_DF where model_type='clean'")
+    return my_cur.fetchall()
+clean_count=get_clean_count()
+clean_count=pd.Dataframe(clean_count)
 st.title("DBT-Snowflake Dashboard")
 col1, col2, col3 = st.columns(3)
-col1.metric("Clean", "70 °F", "1.2 °F")
+col1.metric("Clean", len(clean_count), "1.2 °F")
 col2.metric("Base", "9 mph", "-8%")
 col3.metric("Enterprise", "86%", "4%")
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
