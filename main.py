ac999834c7ad402a30b9155a1fc68301e38c49d3
hemanthboot-2019 <49744737+hemanthboot-2019@users.noreply.github.com>
2022-06-06 18:05:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 
+import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
+streamlit.set_page_config(
+     page_title="AI-Support",
+     page_icon="❄️",
+     layout="wide",
+     initial_sidebar_state="expanded",
+     menu_items={
+         'Get Help': 'https://www.extremelycoolapp.com/help',
+         'Report a bug': "https://www.extremelycoolapp.com/bug",
+         'About': "# This is a header. This is an *extremely* cool app!"
+     }
+ )
