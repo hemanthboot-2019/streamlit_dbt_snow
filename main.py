9613d8510ea8a2e0c51ce8c094ca59b0df9efe30
hemanthboot-2019 <49744737+hemanthboot-2019@users.noreply.github.com>
2022-06-06 19:06:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,10 @@ streamlit.set_page_config(
      }
  )
 streamlit.title("DBT-Snowflake Dashboard")
+col1, col2, col3 = st.columns(3)
+col1.metric("Temperature", "70 °F", "1.2 °F")
+col2.metric("Wind", "9 mph", "-8%")
+col3.metric("Humidity", "86%", "4%")
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 with my_cnx.cursor() as my_cur:
      my_cur.execute("select * from DEV_RAW.PUBLIC.DBT_MAPPING_DF")
