13e7d6efd4d1ed1c437002b5317b4c257682436b
hemanthboot-2019 <49744737+hemanthboot-2019@users.noreply.github.com>
2022-06-06 19:07:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 
-import streamlit
+import streamlit as st
 import pandas as pd
 import requests
 import snowflake.connector
 from urllib.error import URLError
 
-streamlit.set_page_config(
+st.set_page_config(
      page_title="AI-Support",
      page_icon="❄️",
      layout="wide",
@@ -16,7 +16,7 @@ streamlit.set_page_config(
          'About': "# This is a header. This is an *extremely* cool app!"
      }
  )
-streamlit.title("DBT-Snowflake Dashboard")
+st.title("DBT-Snowflake Dashboard")
 col1, col2, col3 = st.columns(3)
 col1.metric("Temperature", "70 °F", "1.2 °F")
 col2.metric("Wind", "9 mph", "-8%")
@@ -24,4 +24,4 @@ col3.metric("Humidity", "86%", "4%")
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 with my_cnx.cursor() as my_cur:
      my_cur.execute("select * from DEV_RAW.PUBLIC.DBT_MAPPING_DF")
-     streamlit.dataframe(my_cur.fetchall())
+     st.dataframe(my_cur.fetchall())
