1925542a258982a12a36f8b4f8870541ae13806b
hemanthboot-2019 <49744737+hemanthboot-2019@users.noreply.github.com>
2022-06-07 22:35:44 +0530

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -90,6 +90,6 @@ with my_cnx.cursor() as my_cur:
                result=result+x+"','"
           st.text(result)
           #my_cur.execute("select model_name, model_ref_by from DEV_RAW.PUBLIC.DBT_MAPPING where model_type='"+model_type_opt+"' and model_business='"+model_business_opt+"'and model_name in ("+model_list+")")
-          st.dataframe( "select model_name, model_ref_by from DEV_RAW.PUBLIC.DBT_MAPPING where model_type='"+model_type_opt+"' and model_business='"+model_business_opt+"'and model_name in ("+result+")")
+          st.text( "select model_name, model_ref_by from DEV_RAW.PUBLIC.DBT_MAPPING where model_type='"+model_type_opt+"' and model_business='"+model_business_opt+"'and model_name in ("+result+")")
      else :
         st.text("button not clicked")
