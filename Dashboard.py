5c2c77f2fe916a9458ac6515d417f9e898173cbf
hemanthboot-2019 <49744737+hemanthboot-2019@users.noreply.github.com>
2022-06-08 00:39:00 +0530

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -97,7 +97,7 @@ with my_cnx.cursor() as my_cur:
           df=pd.DataFrame(my_cur.fetchall())
           df.columns = ["model_name"]
           st.text(df)
-          list_ref=df.['model_name'].tolist()
+          list_ref=df['model_name'].tolist()
           st.text(list_ref)
           #st.text( "select model_name, model_ref_by from DEV_RAW.PUBLIC.DBT_MAPPING where model_type='"+model_type_opt+"' and model_business='"+model_business_opt+"'and model_name in ("+result+")")
      else :
