52bf5ac15848bfb3975f8f14af00bdbd99165466
hemanthboot-2019 <49744737+hemanthboot-2019@users.noreply.github.com>
2022-06-07 23:28:01 +0530

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -95,6 +95,7 @@ with my_cnx.cursor() as my_cur:
           #my_cur.execute("select model_ref_by,model_name  from DEV_RAW.PUBLIC.DBT_MAPPING where model_ref_by in ("+result+")")
           my_cur.execute(" select model_name  from DEV_RAW.PUBLIC.DBT_MAPPING where model_ref_by in ("+result+")")
           df=pd.DataFrame(my_cur.fetchall())
+          st.text(df)
           list_ref=', '.join(f'\'{w}\'' for w in df)
           st.text(list_ref)
           #st.text( "select model_name, model_ref_by from DEV_RAW.PUBLIC.DBT_MAPPING where model_type='"+model_type_opt+"' and model_business='"+model_business_opt+"'and model_name in ("+result+")")
