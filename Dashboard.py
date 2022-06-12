9cf923e88a981550efb4eb26c8699df1d89acef4
hemanthboot-2019 <49744737+hemanthboot-2019@users.noreply.github.com>
2022-06-13 00:58:39 +0530

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -42,9 +42,42 @@ with st.sidebar:
 @st.cache  
 def get_model_count():
      with my_cnx.cursor() as my_cur:
-          my_cur.execute("select model_type,count(distinct model_name)  from DEV_RAW.PUBLIC.DBT_MAPPING group by model_type")
+          my_cur.execute("select model_type,count(distinct model_name) from DEV_RAW.PUBLIC.DBT_MAPPING group by model_type")
           return my_cur.fetchall()
-     
+@st.cache
+def get_model_type():
+     with my_cnx.cursor() as my_cur:
+          my_cur.execute("select distinct model_type from DEV_RAW.PUBLIC.DBT_MAPPING order by model_type")
+          return my_cur.fetchall()
+@st.cache
+def get_model_business_by_type(model_type):
+     with my_cnx.cursor() as my_cur:
+          my_cur.execute("select distinct model_business from DEV_RAW.PUBLIC.DBT_MAPPING where model_type='"+model_type+"' order by model_business")
+          return my_cur.fetchall()
+@st.cache
+def get_model_name_by_business(model_type,model_business):
+     with my_cnx.cursor() as my_cur:
+          my_cur.execute("select distinct model_name from DEV_RAW.PUBLIC.DBT_MAPPING where model_type='"+model_type+"' and model_business='"+model_business+"' order by model_name")
+          return my_cur.fetchall()
+@st.cache     
+def dag(input_array):
+          objects=', '.join(f'\'{w}\'' for w in input_array)
+          with my_cnx.cursor() as my_cur:
+               my_cur.execute(" select distinct model_name  from DEV_RAW.PUBLIC.DBT_MAPPING where model_ref_by in ("+objects+")")
+               df=pd.DataFrame(my_cur.fetchall())
+               if len(df.columns)>0:
+                    df.columns = ["model_name"]
+                    list_ref=df['model_name'].tolist()
+                    my_cur.execute(" select distinct model_name,model_ref_by  from DEV_RAW.PUBLIC.DBT_MAPPING where model_ref_by in ("+objects+")")
+                    df = pd.DataFrame(my_cur.fetchall())
+                    df.columns = ["model_name","model_ref_by"]
+                    df = df.reset_index() 
+                    for index, row in df.iterrows():
+                         graph.edge(row['model_ref_by'], row['model_name'])
+                         res.append(row['model_name'])
+                    if len(df)>0:
+                         dag(list_ref)
+               return "end"     
 if tabs == 'Dashboard':
      my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
      st.title("DBT-Snowflake Dashboard")
@@ -73,140 +106,114 @@ if tabs == 'Dashboard':
      col3.metric("Outbound", outbound_count, "4%")
                                           
 elif tabs=='Impact':
-     def dag(input_array):
-          objects=', '.join(f'\'{w}\'' for w in input_array)
-          with my_cnx.cursor() as my_cur:
-               my_cur.execute(" select distinct model_name  from DEV_RAW.PUBLIC.DBT_MAPPING where model_ref_by in ("+objects+")")
-               df=pd.DataFrame(my_cur.fetchall())
-               if len(df.columns)>0:
-                    df.columns = ["model_name"]
-                    list_ref=df['model_name'].tolist()
-                    my_cur.execute(" select distinct model_name,model_ref_by  from DEV_RAW.PUBLIC.DBT_MAPPING where model_ref_by in ("+objects+")")
-                    df = pd.DataFrame(my_cur.fetchall())
-                    df.columns = ["model_name","model_ref_by"]
-                    df = df.reset_index() 
-                    for index, row in df.iterrows():
-                         graph.edge(row['model_ref_by'], row['model_name'])
-                         res.append(row['model_name'])
-                    if len(df)>0:
-                         dag(list_ref)
-               return "end"
+     
+     col1, col2,col3 = st.columns(3)
+     with col1:
+          model_type=pd.DataFrame(get_model_type())
+          model_type_opt=st.selectbox("Model Type",(model_type))
+     with col2:
+          model_business=pd.DataFrame(get_model_business_by_type(model_type_opt))
+          model_business_opt=st.selectbox("Model Business",(model_business))
+     with col3:
+          model_list=pd.DataFrame(get_model_name_by_business(model_type_opt,model_business_opt))
+          model_list_opt=st.multiselect("Model List",(model_list))
+ 
+     col0,col1,col2,col3 =st.columns(4)
+     with col0:
+          st.text('Materialization :')
+     with col1:
+          view = st.checkbox('View',value=True)
+     with col2:
+          table = st.checkbox('Table',value=True)
+     with col3:
+          incremental = st.checkbox('Incremental',value=True)
+     materialize=[]
+     if view:
 
-     my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
-     with my_cnx.cursor() as my_cur:
-          my_cur.execute("select distinct model_type  from DEV_RAW.PUBLIC.DBT_MAPPING order by model_type")
-          model_type=pd.DataFrame(my_cur.fetchall())
-          col1, col2,col3 = st.columns(3)
+          materialize.append('view')
+     if table:
+          materialize.append('table')
+     if incremental:
+          materialize.append('incremental')
+     material=', '.join(f'\'{w}\'' for w in materialize)      
+     #st.text(material)
+     if len(model_list_opt):
+          result=', '.join(f'\'{w}\'' for w in model_list_opt)         
+          my_cur.execute(" select distinct model_name  from DEV_RAW.PUBLIC.DBT_MAPPING where model_ref_by in ("+result+")")
+          df=pd.DataFrame(my_cur.fetchall())
+          df.columns = ["model_name"]
+          list_ref=df['model_name'].tolist()
+          res=[]
+          graph = graphviz.Digraph()
+
+          with st.spinner('Processing ...'):
+               dag(model_list_opt)
+               st.graphviz_chart(graph)
+               #st.text(res)
+          result=', '.join(f'\'{w}\'' for w in res) 
+
+
+          col1,col2,col3 =st.columns(3)
           with col1:
-               model_type_opt=st.selectbox("Model Type",(model_type))
-               st.text(model_type_opt)
+               st.text('CLEAN')
+               my_cur.execute(" select distinct nvl(model_name,'NA')  from DEV_RAW.PUBLIC.DBT_MAPPING where model_name in ("+result+") and model_type ='clean' and CONFIG_MATERIALIZATION in ("+material+")")
+               st.dataframe(my_cur.fetchall())
           with col2:
-               my_cur.execute("select distinct model_business from DEV_RAW.PUBLIC.DBT_MAPPING where model_type='"+model_type_opt+"' order by model_business")
-               model_business=pd.DataFrame(my_cur.fetchall())
-               model_business_opt=st.selectbox("Model Business",(model_business))
-               st.text(model_business_opt)
+               st.text('BASE')
+               my_cur.execute(" select distinct nvl(model_name,'NA')  from DEV_RAW.PUBLIC.DBT_MAPPING where model_name in ("+result+") and model_type ='base' and CONFIG_MATERIALIZATION in ("+material+")")
+               st.dataframe(my_cur.fetchall())
           with col3:
-               my_cur.execute("select distinct model_name  from DEV_RAW.PUBLIC.DBT_MAPPING where model_type='"+model_type_opt+"' and model_business='"+model_business_opt+"' order by model_name")
-               model_list=pd.DataFrame(my_cur.fetchall())
-               model_list_opt=st.multiselect("Model List",(model_list))
-               st.text(model_list_opt)
-
-          col0,col1,col2,col3 =st.columns(4)
-          with col0:
-               st.text('Materialization :')
-          with col1:
-               view = st.checkbox('View',value=True)
-          with col2:
-               table = st.checkbox('Table',value=True)
-          with col3:
-               incremental = st.checkbox('Incremental',value=True)
-          materialize=[]
-          if view:
-
-               materialize.append('view')
-          if table:
-               materialize.append('table')
-          if incremental:
-               materialize.append('incremental')
-          material=', '.join(f'\'{w}\'' for w in materialize)      
-          #st.text(material)
-          if len(model_list_opt):
-               result=', '.join(f'\'{w}\'' for w in model_list_opt)         
-               my_cur.execute(" select distinct model_name  from DEV_RAW.PUBLIC.DBT_MAPPING where model_ref_by in ("+result+")")
-               df=pd.DataFrame(my_cur.fetchall())
-               df.columns = ["model_name"]
-               list_ref=df['model_name'].tolist()
-               res=[]
-               graph = graphviz.Digraph()
-
-               with st.spinner('Processing ...'):
-                    dag(model_list_opt)
-                    st.graphviz_chart(graph)
-                    #st.text(res)
-               result=', '.join(f'\'{w}\'' for w in res) 
-
-
+               st.text('MDL')
+               my_cur.execute(" select distinct nvl(model_name,'NA')  from DEV_RAW.PUBLIC.DBT_MAPPING where model_name in ("+result+") and model_type ='mdl' and CONFIG_MATERIALIZATION in ("+material+")")
+               st.dataframe(my_cur.fetchall())
+          col4,col5,col6 =st.columns(3)
+          with col4:
+               st.text('AGGREGATE')
+               my_cur.execute(" select distinct nvl(model_name,'NA')  from DEV_RAW.PUBLIC.DBT_MAPPING where model_name in ("+result+") and model_type ='aggregate' and CONFIG_MATERIALIZATION in ("+material+")")
+               st.dataframe(my_cur.fetchall())
+          with col5:
+               st.text('OUTBOUND')
+               my_cur.execute(" select distinct nvl(model_name,'NA')  from DEV_RAW.PUBLIC.DBT_MAPPING where model_name in ("+result+") and model_type ='outbound_sds' and CONFIG_MATERIALIZATION in ("+material+")")
+               st.dataframe(my_cur.fetchall())
+          with col6:
+               st.text('ENTERPRISE')
+               my_cur.execute(" select distinct nvl(model_name,'NA')  from DEV_RAW.PUBLIC.DBT_MAPPING where model_name in ("+result+") and model_type ='enterprise' and CONFIG_MATERIALIZATION in ("+material+")")
+               st.dataframe(my_cur.fetchall())
+          my_cur.execute(" select distinct nvl(model_name,'NA')  from DEV_RAW.PUBLIC.DBT_MAPPING where model_name in ("+result+") and CONFIG_MATERIALIZATION in ("+material+")")
+          df = pd.DataFrame(my_cur.fetchall())
+          df.columns = ["model_name"]
+          df = df.reset_index() 
+          run_list=[]
+          full_list=['']
+          for index, row in df.iterrows():
+               model=row['model_name']
                col1,col2,col3 =st.columns(3)
                with col1:
-                    st.text('CLEAN')
-                    my_cur.execute(" select distinct nvl(model_name,'NA')  from DEV_RAW.PUBLIC.DBT_MAPPING where model_name in ("+result+") and model_type ='clean' and CONFIG_MATERIALIZATION in ("+material+")")
-                    st.dataframe(my_cur.fetchall())
+                    st.text(model)
                with col2:
-                    st.text('BASE')
-                    my_cur.execute(" select distinct nvl(model_name,'NA')  from DEV_RAW.PUBLIC.DBT_MAPPING where model_name in ("+result+") and model_type ='base' and CONFIG_MATERIALIZATION in ("+material+")")
-                    st.dataframe(my_cur.fetchall())
+                    run_model = st.checkbox('run',key=model)
+                    if run_model:
+                         run_list.append(model)
                with col3:
-                    st.text('MDL')
-                    my_cur.execute(" select distinct nvl(model_name,'NA')  from DEV_RAW.PUBLIC.DBT_MAPPING where model_name in ("+result+") and model_type ='mdl' and CONFIG_MATERIALIZATION in ("+material+")")
-                    st.dataframe(my_cur.fetchall())
-               col4,col5,col6 =st.columns(3)
-               with col4:
-                    st.text('AGGREGATE')
-                    my_cur.execute(" select distinct nvl(model_name,'NA')  from DEV_RAW.PUBLIC.DBT_MAPPING where model_name in ("+result+") and model_type ='aggregate' and CONFIG_MATERIALIZATION in ("+material+")")
-                    st.dataframe(my_cur.fetchall())
-               with col5:
-                    st.text('OUTBOUND')
-                    my_cur.execute(" select distinct nvl(model_name,'NA')  from DEV_RAW.PUBLIC.DBT_MAPPING where model_name in ("+result+") and model_type ='outbound_sds' and CONFIG_MATERIALIZATION in ("+material+")")
-                    st.dataframe(my_cur.fetchall())
-               with col6:
-                    st.text('ENTERPRISE')
-                    my_cur.execute(" select distinct nvl(model_name,'NA')  from DEV_RAW.PUBLIC.DBT_MAPPING where model_name in ("+result+") and model_type ='enterprise' and CONFIG_MATERIALIZATION in ("+material+")")
-                    st.dataframe(my_cur.fetchall())
-               my_cur.execute(" select distinct nvl(model_name,'NA')  from DEV_RAW.PUBLIC.DBT_MAPPING where model_name in ("+result+") and CONFIG_MATERIALIZATION in ("+material+")")
-               df = pd.DataFrame(my_cur.fetchall())
-               df.columns = ["model_name"]
-               df = df.reset_index() 
-               run_list=[]
-               full_list=['']
-               for index, row in df.iterrows():
-                    model=row['model_name']
-                    col1,col2,col3 =st.columns(3)
-                    with col1:
-                         st.text(model)
-                    with col2:
-                         run_model = st.checkbox('run',key=model)
-                         if run_model:
-                              run_list.append(model)
-                    with col3:
-                         full_model = st.checkbox('full_refresh',key=model+'_f',value=False)
-                         if full_model:
-                              full_list.append(model)
-                         
-               #st.text(run_list)
-               #st.text(full_list)
-               dbt_run= 'dbt run --models '
-               dbt_full_run= 'dbt run --models'
-               for i in run_list:
-                    for j in full_list:
-                         if j==i:
-                              dbt_full_run = dbt_full_run +' '+i
-                         else:
-                              dbt_run=dbt_run+' '+i
-               dbt_full_run = dbt_full_run +' --full-refresh'
-               #st.text('DBT RUN :'+dbt_run)
-               st.code(dbt_run, language='python')
-               st.code(dbt_full_run, language='python')
-               #st.text('DBT FULL RUN :'+dbt_full_run)
+                    full_model = st.checkbox('full_refresh',key=model+'_f',value=False)
+                    if full_model:
+                         full_list.append(model)
+
+          #st.text(run_list)
+          #st.text(full_list)
+          dbt_run= 'dbt run --models '
+          dbt_full_run= 'dbt run --models'
+          for i in run_list:
+               for j in full_list:
+                    if j==i:
+                         dbt_full_run = dbt_full_run +' '+i
+                    else:
+                         dbt_run=dbt_run+' '+i
+          dbt_full_run = dbt_full_run +' --full-refresh'
+          #st.text('DBT RUN :'+dbt_run)
+          st.code(dbt_run, language='python')
+          st.code(dbt_full_run, language='python')
+          #st.text('DBT FULL RUN :'+dbt_full_run)
                
                     
                
